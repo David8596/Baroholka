15d0a26c39d6a297cef5658333391d3aadf0300e
THEsTAS-cyber <stassariew19@gmail.com>
2025-06-29 13:37:15 +0300

    f

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import aiogram
-import time
+from bot_client import bot
+from dispatcher import Dispatcher
+import asyncio
 
-print('Hello')
-print("hello2")
+dp = Dispatcher()
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == '__main__':
+    asyncio.run(main())
